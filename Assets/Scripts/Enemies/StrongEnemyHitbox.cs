5c9d2e2c23b71035bc4ef9f1f7d56f2a2e2c6daa
wmckay555 <67658851+wmckay555@users.noreply.github.com>
2021-11-16 17:07:00 -0800

    started working on strong enemy

---
+++ Assets/Scripts/Enemies/StrongEnemyHitbox.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StrongEnemyHitbox : MonoBehaviour
+{
+    public float ActiveTime;
+    public float windup;
+
+    public int Damage;
+
+    public Transform Anchor;
+
+    public float Timer;
+
+    public float KnockbackPower;
+
+    public Transform Owner;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, ActiveTime + windup);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer -= Time.deltaTime;
+
+        if (Timer <= ActiveTime)
+        {
+            transform.Rotate(Vector3.up, speed * Time.deltaTime);
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerHP>().TakeDamage(Damage, KnockbackPower, Owner.transform.position);
+
+        }
+    }
+}
