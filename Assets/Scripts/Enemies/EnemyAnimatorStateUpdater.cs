79f17ce3485e9c6307a381900254aaf02ab8b53e
Johan Östling <johan.f.ostling@gmail.com>
2022-01-05 11:55:09 +0100

    Add animated visible alert status for enemies

--- Assets/Scripts/Enemies/EnemyAnimatorStateUpdater.cs
+++ Assets/Scripts/Enemies/EnemyAnimatorStateUpdater.cs
@@ -1,8 +1,11 @@
+using System.Linq;
 using UnityEngine;
 
 public class EnemyAnimatorStateUpdater : MonoBehaviour
 {
-    public Animator animator;
+    [HideInInspector] public Animator animator;
+    public Animator[] animators;
+
     private static readonly int Alerted = Animator.StringToHash("Alerted");
     private static readonly int Attacking = Animator.StringToHash("Attacking");
     private static readonly int SeePlayer = Animator.StringToHash("SeePlayer");
@@ -10,13 +13,34 @@ public class EnemyAnimatorStateUpdater : MonoBehaviour
     private static readonly int Pausing = Animator.StringToHash("Pausing");
     private static readonly int Retreating = Animator.StringToHash("Retreating");
 
+    private void Start()
+    {
+        // Make the animators array backwards compatible with the animator field
+        if (animators == null || animators.Length == 0)
+        {
+            animators = new[] {animator};
+        }
+    }
+
     public void UpdateAnimator(EnemyState state)
     {
-        animator.SetBool(Alerted, state.Alerted);
-        animator.SetBool(Attacking, state.Attacking);
-        animator.SetBool(SeePlayer, state.SeePlayer);
-        animator.SetBool(Chasing, state.Chasing);
-        animator.SetBool(Pausing, state.Pausing);
-        animator.SetBool(Retreating, state.Retreating);
+        SetBool(Alerted, state.Alerted);
+        SetBool(Attacking, state.Attacking);
+        SetBool(SeePlayer, state.SeePlayer);
+        SetBool(Chasing, state.Chasing);
+        SetBool(Pausing, state.Pausing);
+        SetBool(Retreating, state.Retreating);
+    }
+
+    private void SetBool(int id, bool state)
+    {
+        foreach (var a in animators)
+        {
+            // Check if this animator has this parameter before setting it
+            if (a.parameters.Any(p => p.nameHash == id))
+            {
+                a.SetBool(id, state);
+            }
+        }
     }
 }
